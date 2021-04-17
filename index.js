7364626160671d37ba275db723e72960c2ce49ac
kurtor112 <kurt.or@outlook.com>
2021-04-17 10:38:10 +0800

    3 files modified

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ import "./style.css";
 // const appDiv = document.getElementById('app');
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-document.onclick = userClicked;
-function userClicked() {
-  var x = event.clientX;
-  var y = event.clientY;
-  var snowball = document.getElementById("snowballAppear");
-  snowball.style.display = "";
-  snowball.style.position = "absolute";
-  snowball.style.left = x + "px";
-  snowball.style.top = y + "px";
-}
+// document.onclick = userClicked;
+// function userClicked() {
+//   var x = event.clientX;
+//   var y = event.clientY;
+//   var snowball = document.getElementById("snowballAppear");
+//   snowball.style.display = "";
+//   snowball.style.position = "absolute";
+//   snowball.style.left = x + "px";
+//   snowball.style.top = y + "px";
+// }
